7231ec27ee1c67402910b21da84d900b51a30dbb
Valery Statinov <valery.statinov@gmail.com>
2020-10-15 22:50:07 +0300

    update presentation, clock solution

--- seminar_4_Life-cycle/examples/clock/src/App.js
+++ seminar_4_Life-cycle/examples/clock/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 const Clock = ({ date }) => <div>{date.toLocaleTimeString()}</div>
 
 class App extends React.Component {
+  state = {
+    time: new Date()
+  }
+  interval = null
+
+  componentDidMount () {
+    this.interval = setInterval(() => {
+      this.setState({ time: new Date() })
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval)
+  }
+
   render () {
     return (
       <div>
         <div>Current time is</div>
-        <Clock date={new Date()} />
+        <Clock date={this.state.time} />
       </div>
     )
   }
